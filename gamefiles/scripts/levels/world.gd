aca44097ae0c8832669b11779001541b4cdacd89
Nabir14 <82253045+Nabir14@users.noreply.github.com>
2026-03-30 01:28:07 +0600

    Farm System - Complete (godot)

--- gamefiles/scripts/levels/world.gd
+++ gamefiles/scripts/levels/world.gd
@@ -1,2 +1,21 @@
 extends Node2D
 class_name World
+
+@export var farm_manager : FarmManager
+@export var game_tick : Timer
+
+func _ready() -> void:
+	game_tick.timeout.connect(_on_game_tick)
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("mouse_action"):
+		var mouse_position : Vector2 = get_global_mouse_position()
+		var tile_position : Vector2 = farm_manager.to_farm_position(mouse_position)
+		
+		if farm_manager.is_tile_empty(tile_position):
+			farm_manager.place_crop(tile_position, load("res://gamefiles/resources/crops/carrot.tres"))
+		elif farm_manager.is_tile_ready_for_harvest(tile_position):
+			farm_manager.harvest_crop_at_tile(tile_position)
+
+func _on_game_tick() -> void:
+	farm_manager.update_crops()
--- gamefiles/scripts/managers/farm_manager.gd
+++ gamefiles/scripts/managers/farm_manager.gd
@@ -1,6 +1,9 @@
 extends Node2D
 class_name FarmManager
 
+signal crop_planted(crop : Crop, tile_position : Vector2i)
+signal crop_harvested(crop : Crop, tile_position : Vector2i)
+
 @export_group("Farm Setup")
 @export var farm_tilemap_layer : TileMapLayer
 @export var crop_tilemap_layer : TileMapLayer
@@ -16,13 +19,26 @@ func place_crop(tile_position : Vector2i, crop : Crop) -> void:
 			"position": tile_position,
 			"current_age": 0
 		})
+		
+		crop_tilemap_layer.set_cell(tile_position, crop_source_id, crop.age_sprites[0])
+		
+		crop_planted.emit(crop, tile_position)
+
+func harvest_crop_at_tile(tile_position : Vector2i) -> void:
+	for crop in placed_crops:
+		if crop.position == tile_position:
+			placed_crops.erase(crop)
+		crop_tilemap_layer.erase_cell(tile_position)
+		
+		crop_harvested.emit(crop.resource, tile_position)
+	
 
 func update_crops() -> void:
 	for crop in placed_crops:
-		if crop.current_age >= crop.resource.max_age:
-			crop_tilemap_layer.set_cell(crop.position, crop_source_id, crop.age_sprites[crop.resource.age_sprites.size() - 1])
+		if is_crop_ready_for_harvest(crop.current_age, crop.resource.max_age):
+			crop_tilemap_layer.set_cell(crop.position, crop_source_id, crop.resource.age_sprites[crop.resource.age_sprites.size() - 1])
 		else:
-			crop_tilemap_layer.set_cell(crop.position, crop_source_id, crop.age_sprites[0])
+			crop_tilemap_layer.set_cell(crop.position, crop_source_id, crop.resource.age_sprites[0])
 		
 		crop.current_age += 1
 
@@ -32,8 +48,32 @@ func is_tile_farm_land(tile_position : Vector2i) -> bool:
 	else:
 		return false
 
-func is_tile_empty(tile_position : Vector2) -> bool:
+func is_tile_empty(tile_position : Vector2i) -> bool:
+	var result : bool = false
+	
 	if crop_tilemap_layer.get_cell_atlas_coords(tile_position) == Vector2i(-1, -1):
+		result = true
+	
+	for crop in placed_crops:
+		if crop.position == tile_position:
+			result = false
+	
+	return result
+
+func is_crop_ready_for_harvest(current_age : int, max_age : int) -> bool:
+	if current_age >= max_age:
 		return true
 	else:
 		return false
+
+func is_tile_ready_for_harvest(tile_position : Vector2i) -> bool:
+	for crop in placed_crops:
+		if crop.position == tile_position:
+			if is_crop_ready_for_harvest(crop.current_age, crop.resource.max_age):
+				return true
+			else:
+				return false
+	return false
+
+func to_farm_position(local_position : Vector2) -> Vector2i:
+	return farm_tilemap_layer.local_to_map(local_position)
